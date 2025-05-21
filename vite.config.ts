3bd492e1a3e7e344a2066a4f118b518c49386529
maher boughdiri <maher.boughdiri@softtodo.com>
2025-05-21 09:21:18 +0200

    [TASK] add publish configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-  css: {
-    preprocessorOptions: {
-      // optional if you're using SCSS too
-    },
-  },
+  plugins: [react(), tailwindcss()],
   build: {
     lib: {
-      entry: './src/components/index.ts',
-      name: 'mb-react-ui-kit',
+      entry: "./src/components/index.ts",
+      name: "MbReactUiKit",
+      formats: ["es", "umd"],
+      fileName: (format) => `mb-react-ui-kit.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
-  }
-})
+  },
+});
